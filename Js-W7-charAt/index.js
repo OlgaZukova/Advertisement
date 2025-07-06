b4b60ad651f7ca5cfcaf593c5e70a5ef1ac34682
OlgaZukova <125801372+OlgaZukova@users.noreply.github.com>
2025-07-06 12:48:50 +0100

    Add files via upload

---
+++ Js-W7-charAt/index.js
@@ -0,0 +1,13 @@
+let text = "I am a web developer..."; //23
+//console.log(text.length)
+let i = 0;
+let speed = 100;
+
+function type(){
+    if (i<text.length){
+        document.querySelector("#par").textContent += text.charAt(i);
+        i++
+        setTimeout(type, speed)
+    }
+}
+type();
